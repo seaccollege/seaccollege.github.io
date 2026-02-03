b77bb04d9163f29c79db3144e8e906686cf3683b
South Eastern Arabic College <seaccollege@gmail.com>
2026-02-03 22:06:59 +0530

    Bug Fix

---
+++ src/components/angular/admission-offline/admission-offline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admission-offline',
+  templateUrl: './admission-offline.component.html',
+  styleUrls: ['./admission-offline.component.scss']
+})
+export class AdmissionOfflineComponent {
+
+}
