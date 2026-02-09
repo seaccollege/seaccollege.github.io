212a8153514433f1e43172799a759ea289785955
South Eastern Arabic College <seaccollege@gmail.com>
2026-02-09 22:37:06 +0530

    Admission Part Update with Authentication

--- src/components/links/GitHubLink.svelte
+++ src/components/links/GitHubLink.svelte
@@ -1,9 +1,13 @@
-<a
-    href="https://github.com/seaccollege"
-    class="p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700"
-    aria-label="SEAC GitHub"
-    target="_blank"
-    rel="noopener noreferrer"
->
-    <img src="/3rd-party/github-mark.svg" alt="GitHub Icon" title="GitHub" width="24" height="24" loading="lazy" />
-</a>
+<p>
+    Made with ❤️ by
+    <a
+        href="https://github.com/seaccollege"
+        class="p-2 rounded-md text-pink-400 hover:text-pink-300 dark:hover:text-pink-700"
+        aria-label="SEAC GitHub"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        <!-- <img src="/3rd-party/github-mark.svg" alt="GitHub Icon" title="GitHub" width="24" height="24" loading="lazy" /> -->
+        SEAC College
+    </a>
+</p>
